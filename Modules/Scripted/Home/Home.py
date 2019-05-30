BICCN/cell-locator

commit 605d23b3cb604ede9ece93fd93f836c3eabfd3a6

ENH: Update CTK and add support for applying yaw/pitch/roll when Enter is pressed
See #67

List of CTK changes:

$ git shortlog e6aabb5e..118a302f --no-merges
Jean-Christophe Fillion-Robin (2):
      [cell-locator] ENH: ctkSlicerWidget: Expose popup()/spinBox()/slider() to python scripting
      [cell-locator] ENH: ctkDoubleSlider: Add returnPressed() signal

CMakeLists.txt
@@ -114,7 +114,7 @@ set(Slicer_EXTENSION_SOURCE_DIRS
 
 # Custom CTK
 set(Slicer_CTK_GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/KitwareMedical/CTK.git")
-set(Slicer_CTK_GIT_TAG "e6aabb5e4bfb0d86037263f7f1d39170716be933") # cell-locator-2018-11-20-6cd82aab
+set(Slicer_CTK_GIT_TAG "118a302f688df3e7a151d354fab4cda6b58d1ea2") # cell-locator-2018-11-20-6cd82aab
 
 # Add Slicer sources
 add_subdirectory(${slicersources_SOURCE_DIR} ${slicersources_BINARY_DIR})

Modules/Scripted/Home/Home.py
@@ -979,6 +979,8 @@ class HomeWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
     for axe in ['Roll', 'Yaw', 'Pitch']:
       self.get('%sSliderWidget' % axe).connect("valueChanged(double)", setAdjustAndResetButtonsEnabled)
+      self.get('%sSliderWidget' % axe).spinBox().connect("editingFinished()", self.onViewOrientationChanged)
+      self.get('%sSliderWidget' % axe).slider().connect("returnPressed()", self.onViewOrientationChanged)
     self.get('AdjustViewPushButton').connect("clicked()", self.onViewOrientationChanged)
 
     self.get('StepSizeSliderWidget').connect("valueChanged(double)", self.onStepSizeChanged)

commit 4d6263779d23d291154672474eaa801fc0d9682a

BUG: Ensure entering Roll/Pitch/Yaw values does NOT automatically Apply. See #75
Fix regression introduced in 605d23b originally addressing #67

List of CTK changes:

$ git shortlog 118a302f..ceed7350 --no-merges
Jean-Christophe Fillion-Robin (1):
      [cell-locator] ENH: ctkDoubleSpinBox: Expose lineEdit() to python scripting

CMakeLists.txt
@@ -114,7 +114,7 @@ set(Slicer_EXTENSION_SOURCE_DIRS
 
 # Custom CTK
 set(Slicer_CTK_GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/KitwareMedical/CTK.git")
-set(Slicer_CTK_GIT_TAG "118a302f688df3e7a151d354fab4cda6b58d1ea2") # cell-locator-2018-11-20-6cd82aab
+set(Slicer_CTK_GIT_TAG "ceed735068db56b8445c04572b85dbfc3c6bb496") # cell-locator-2018-11-20-6cd82aab
 
 # Add Slicer sources
 add_subdirectory(${slicersources_SOURCE_DIR} ${slicersources_BINARY_DIR})

Modules/Scripted/Home/Home.py
@@ -985,7 +985,7 @@ class HomeWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
     for axe in ['Roll', 'Yaw', 'Pitch']:
       self.get('%sSliderWidget' % axe).connect("valueChanged(double)", setAdjustAndResetButtonsEnabled)
-      self.get('%sSliderWidget' % axe).spinBox().connect("editingFinished()", self.onViewOrientationChanged)
+      self.get('%sSliderWidget' % axe).spinBox().lineEdit().connect("returnPressed()", self.onViewOrientationChanged)
       self.get('%sSliderWidget' % axe).slider().connect("returnPressed()", self.onViewOrientationChanged)
     self.get('AdjustViewPushButton').connect("clicked()", self.onViewOrientationChanged)
 