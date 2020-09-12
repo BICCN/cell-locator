BICCN/cell-locator

commit 4eb4eed60057867ddbbc9aa243197ff0c9dbd6cb

BUG: Fix "New" file button.
For https://github.com/BICCN/cell-locator/issues/135

Modules/Scripted/Home/Home.py
@@ -560,10 +560,9 @@ class HomeWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
     if not self.saveIfRequired(): return
 
     self.setInteractionState('explore')
+    self.Annotations.fileName = None
     self.Annotations.clear()
-    annotations = AnnotationManager()
-    self.setAnnotations(annotations)
-    annotations.add()
+    self.Annotations.add()
     self.annotationStored()
 
   def onSaveAnnotationButtonClicked(self):
@@ -608,8 +607,6 @@ class HomeWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
   def loadLIMSSpecimen(self, specimenID):
     logging.info('Loading LIMS specimen id %s', specimenID)
-
-    self.get('AnnotationPathLineEdit').currentPath = 'LIMS Specimen %s' % specimenID
 
     base = slicer.app.commandOptions().limsBaseURL or 'http://localhost:5000/'
     path = '/specimen_metadata/view'
@@ -683,6 +680,11 @@ class HomeWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
     if self.Annotations.current:
       self.Annotations.current.markup.GetStorageNode().StoredModified()
+
+    if slicer.app.commandOptions().limsSpecimenID:
+      self.get('AnnotationPathLineEdit').currentPath = 'LIMS Specimen %s' % specimenID
+    else:
+      self.get('AnnotationPathLineEdit').currentPath = str(self.Annotations.fileName)
 
   def updateCameraFromAnnotations(self):
     viewNode = slicer.app.layoutManager().threeDWidget(0).threeDView().mrmlViewNode()

commit 398896e4a1c04f8c9b77417094ed463151683f37

BUG: Fix LIMS support addressing regression introduced in 4eb4eed
This commit fixes regression introduced in 4eb4eed (BUG: Fix "New" file
button.) originally intended to address #135

Modules/Scripted/Home/Home.py
@@ -721,7 +721,7 @@ class HomeWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
       self.Annotations.current.markup.GetStorageNode().StoredModified()
 
     if slicer.app.commandOptions().limsSpecimenID:
-      self.get('AnnotationPathLineEdit').currentPath = 'LIMS Specimen %s' % specimenID
+      self.get('AnnotationPathLineEdit').currentPath = 'LIMS Specimen %s' % slicer.app.commandOptions().limsSpecimenID
     else:
       self.get('AnnotationPathLineEdit').currentPath = str(self.Annotations.fileName)
 