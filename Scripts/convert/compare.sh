13f8932ac2cccd7005b183426cfeea93f8291c90
David Allemang <david.allemang@kitware.com>
2021-06-15 15:13:14 -0400

    ENH: Add convert.py CLI
    Also add utilities to versions/__init__.py; documented in docstrings.
    
    - version_order
    - infer_normalize
    - match
    - find_latest
    
    ---
    
    Add idempotency test (idem.sh) and updater (update.sh) to demonstrate usage.
    
    All converters are _almost_ idempotent (convert a document to the same; then check the diff of the old document to the new document). Below are the changes.
    
    All converters add `.version` and sometimes cause negligible floating-point errors.
    
    - 0.0.0+2019.01.26
        - adds `.version`
        - resets numbering of `.Markups[].ID`, starting at 0.
        - resets numbering of `.Markups[].AssociatedNodeID`, starting at 0.
    
    - 0.0.0+2020.04.16
        - adds `.version`
    
    - 0.0.0+2020.08.26
        - adds `.version`
        - always sets `.markups[].markup.controlPoints[].selected` to False
        - reduces precision of `.markup[].markup.display.selectedColor`
    
    - 0.1.0+2020.09.18
        - adds `.version`
        - removes `.markups[].markup.measurements`
        - adds `.markups[].markup.coordinateUnits`

---
+++ Scripts/convert/compare.sh
@@ -0,0 +1,16 @@
+# convert the same document to each of the supported versions; to allow comparing outputs
+# results are placed in ./compare/; the original document is copied to ./compare/base.json
+# each other output is named by the target version.
+
+f_in=samples/20170213_297662.003.15.json
+d_out=compare
+
+mkdir -p $d_out
+rm $d_out/*
+
+cp "$f_in" "$d_out/base.json"
+
+for version in $(python convert.py versions); do
+  result="${d_out}/${version}.json"
+  python convert.py convert "$f_in" "$result" -v? -t "$version" -i 2> /dev/null
+done
---
+++ Scripts/convert/convert.py
@@ -0,0 +1,119 @@
+import argparse
+import json
+import sys
+from pathlib import Path
+
+import converters
+
+
+def convert(args):
+    # if src is '-', use stdin
+    if args.src != Path('-'):
+        with open(args.src) as src:
+            data = json.load(src)
+    else:
+        data = json.load(sys.stdin)
+
+    # if -v?, infer version
+    if args.version.lower() in ('?', 'infer'):
+        v, doc = converters.infer_normalize(data)
+        print(f'Inferred version {v!r}', file=sys.stderr)
+    else:
+        v, vc = converters.find_latest(args.version)
+        doc = vc.normalize(data)
+
+    # convert to target version
+    t, tc = converters.find_latest(args.target)
+    data = tc.specialize(doc)
+
+    # convert boolean indent to json.dump argument
+    indent = 2 if args.indent else None
+
+    # if dst is '-', use stdout
+    if args.dst != Path('-'):
+        args.dst.parent.mkdir(exist_ok=True, parents=True)
+        dst = args.dst.open('w')
+    else:
+        dst = sys.stdout
+
+    # dump output
+    json.dump(data, dst, indent=indent)
+
+
+def versions(args):
+    print('\n'.join(converters.match(args.target)))
+
+
+def infer(args):
+    # if src is '-', use stdin
+    if args.src != Path('-'):
+        with open(args.src) as src:
+            data = json.load(src)
+    else:
+        data = json.load(sys.stdin)
+
+    v, doc = converters.infer_normalize(data)
+
+    print(v)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='convert',
+        description=f'Convert Cell Locator annotation files between versions.',
+    )
+
+    subs = parser.add_subparsers(dest='cmd', title='subcommands', required=True)
+
+    sub_convert = subs.add_parser(
+        'convert',
+        help='Convert between file versions.'
+    )
+    sub_convert.add_argument(
+        'src', type=Path,
+        help="Source JSON file. Use '-' to read from stdin.",
+    )
+    sub_convert.add_argument(
+        'dst', type=Path,
+        help="Destination JSON file. Use '-' to write to stdout.",
+    )
+    sub_convert.add_argument(
+        '-v', '--version', required=True,
+        help="Source file version. Use '-v?' to infer the version.",
+    )
+    sub_convert.add_argument(
+        '-t', '--target', default='',
+        help='Target file version. Defaults to the latest version.',
+    )
+    sub_convert.add_argument(
+        '-i', '--indent', action='store_true',
+        help='Indent output JSON.',
+    )
+    sub_convert.set_defaults(func=convert)
+
+    sub_versions = subs.add_parser(
+        'versions',
+        help='Show all versions and exit.',
+    )
+    sub_versions.add_argument(
+        'target', nargs='?', default='',
+        help='Show versions matching this target. If empty, show all versions.',
+    )
+    sub_versions.set_defaults(func=versions)
+
+    sub_infer = subs.add_parser(
+        'infer',
+        help='Show inferred version of file and exit.'
+    )
+    sub_infer.add_argument(
+        'src', type=Path,
+        help="Source JSON file. Use '-' to read from stdin.",
+    )
+    sub_infer.set_defaults(func=infer)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == '__main__':
+    main()
